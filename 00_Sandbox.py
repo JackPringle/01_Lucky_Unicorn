9fd14ca942e4cbfde5740d0648c78fa1a8869d65
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-02-23 10:54:16 +1300

    Add files via upload
    Asks for number, times five

---
+++ 00_Sandbox.py
@@ -0,0 +1,13 @@
+# Ask users for a number
+get_number = int(input("Choose a number? "))
+
+# Multiply the number by 5
+times_five = get_number * 5
+
+answer = "{} time five is equal to " \
+        "{}".format(get_number, times_five)
+
+# Output the result
+print(answer)
+
+
