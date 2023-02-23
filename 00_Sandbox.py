008ef5656887ed7e3aa65209858821ccd3ee23aa
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-02-24 09:52:33 +1300

    Asks for name, exit code

--- 00_Sandbox.py
+++ 00_Sandbox.py
@@ -1,13 +1,10 @@
-# Ask users for a number
-get_number = int(input("Choose a number? "))
+# get name until an exit code is entered...
 
-# Multiply the number by 5
-times_five = get_number * 5
+name = ""
+while name.lower() != "xxx":
+    name = input("Who are you? ")
+    print(name)
 
-answer = "{} time five is equal to " \
-        "{}".format(get_number, times_five)
-
-# Output the result
-print(answer)
-
+print()
+print("We are done!")
 
