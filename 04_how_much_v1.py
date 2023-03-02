fe581df653794cddf90de4159a294b8b2bb491b5
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-03-03 09:49:25 +1300

    Finished v1, vid 11

---
+++ 04_how_much_v1.py
@@ -0,0 +1,23 @@
+# Functions go here
+
+
+# Main routine goes here
+
+error = "Please enter a whole number between 1 and 10\n"
+
+valid = False
+while not valid:
+    try:
+        # Ask the question
+        response = int(input("How much would you like to play with? "))
+
+        # If the amount is too low ? too high give
+        if 0 < response <= 10:
+            print(f"You have asked to play with ${response}")
+
+        # Output an error
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
